622f333757710e0f55058f92d48b9a7ee9c792cf
ramon1martins1 <ramon.martins1@gmail.com>
2025-07-14 14:16:52 -0300

    Fix: Configura repositório do Chrome para instalação correta156

--- wpaut.py
+++ wpaut.py
@@ -7,7 +7,6 @@ from oauth2client.service_account import ServiceAccountCredentials
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
